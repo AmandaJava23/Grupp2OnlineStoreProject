de9413382eb4d7dee52de722272e3fd3a2aca9cc
David Henriksson <davhen0108@skola.goteborg.se>
2024-02-11 14:47:40 +0100

    fixed code in the correct test class heeheeehh

--- OnlineStoreProject/src/test/java/se/yrgo/ProductTest.java
+++ OnlineStoreProject/src/test/java/se/yrgo/ProductTest.java
@@ -1,22 +1,6 @@
 package se.yrgo;
 
-import org.junit.jupiter.api.Test;
-
-import java.util.ArrayList;
-import java.util.List;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductTest {
-    @Test
-    public void orderHistoryTest() {
-        Customer customer = new Customer(1,
-                "David", "david@david.com");
-        assertEquals(0, customer.orderHistory.size());
 
-        List<Product> testOrder = new ArrayList<>();
-        testOrder.add(Product);
-        customer.placeOrder(testOrder);
-        assertEquals(1,customer.orderHistory.size());
-    }
 }
