7e2a0278eaa3a4b774cecfd1305c0dad24076320
Amanda Vasilis <amavas0414@skola.goteborg.se>
2024-02-11 11:12:56 +0100

    adding constructor and methods in ShoppingCart

--- OnlineStoreProject/src/main/java/se/yrgo/ShoppingCart.java
+++ OnlineStoreProject/src/main/java/se/yrgo/ShoppingCart.java
@@ -1,5 +1,37 @@
 package se.yrgo;
 
-public class ShoppingCart {
+import java.util.ArrayList;
+import java.util.List;
 
+public class ShoppingCart {
+    private List<Product> shoppingCart;
+
+    public ShoppingCart() {
+        this.shoppingCart = new ArrayList<>();
+    }
+
+    public void addProductToCart(Product product) {
+        shoppingCart.add(product);
+    }
+
+    public void removeProductFromCart(Product product) {
+        shoppingCart.remove(product);
+    }
+
+    public double valueOfCart() {
+        double valueOfCart = 0;
+        for (Product product : shoppingCart) {
+            valueOfCart = valueOfCart + product.getValue();
+        }
+        return valueOfCart;
+    }
+
+    public void showShoppingCart() {
+        for (Product product : shoppingCart) {
+            System.out.println(product);
+        }
+    }
+    @Override
+    public String toString() { return "Shopping Cart: " + shoppingCart;
+    }
 }
