918d780e0d60a2b964e9dcffc8f4c2e85114b414
5ail3 <saile.dragednas@proton.me>
2024-02-13 14:34:59 +0100

    adding class Product

--- OnlineStoreProject/src/main/java/se/yrgo/Main.java
+++ OnlineStoreProject/src/main/java/se/yrgo/Main.java
@@ -2,7 +2,9 @@ package se.yrgo;
 
 public class Main {
     public static void main(String[] args) {
-
-
+        Product hammer = new Product("hammer", "18293812468A", 150);
+        Product nails = new Product("nails", "891264236897S", 150);
+        Product drillMachine = new Product("drill_machine", "89231749889SD", 300);
+        
     }
 }
--- OnlineStoreProject/src/main/java/se/yrgo/Product.java
+++ OnlineStoreProject/src/main/java/se/yrgo/Product.java
@@ -1,5 +1,27 @@
 package se.yrgo;
 
+/*
+ * Class defining the product class.
+ * 
+ * 
+ */
 public class Product {
+    private String name;
+    private String artNo;
+    private double price;
 
+    public Product() {
+        this.name = name;
+        this.artNo = artNo;
+        this.price = price;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getArtNo() {
+        return artNo;
+    }
+    public double getPrice() {
+        return price;
+    }
 }
